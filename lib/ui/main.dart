d98fe6075ac21f44981fa99d5d32e3274f688cd2
tanifumiya <fumiyadeveloper@gmail.com>
2021-05-03 07:55:53 +0900

    ExpansionPanelで使用するファイル名をスネークケースに変更

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,4 +1,4 @@
-import 'package:cocktail_recipe_app/data/CocktailExpansionPanelItem.dart';
+import 'package:cocktail_recipe_app/ui/cocktail_expansion_panel_item.dart';
 import 'package:flutter/material.dart';
 
 void main() {
