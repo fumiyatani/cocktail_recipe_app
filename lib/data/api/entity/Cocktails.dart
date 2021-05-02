e7700f20a9199ccb07f14db36602694c28a83a7d
tanifumiya <fumiyadeveloper@gmail.com>
2021-05-03 03:46:07 +0900

    JSONからモデルへの変換を行う処理を作成

---
+++ lib/data/api/entity/Cocktails.dart
@@ -0,0 +1,119 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'Cocktails.g.dart';
+
+@JsonSerializable()
+class Cocktails {
+  Cocktails(
+      {this.status, this.totalPages, this.currentPage, this.cocktails});
+
+  /// 正常時：0000, 現状使用するエラーは E001 (wordのフォーマットが不正)
+  @JsonKey(name: 'status')
+  final String status;
+
+  @JsonKey(name: 'total_pages')
+  final int totalPages;
+
+  @JsonKey(name: 'current_page')
+  final int currentPage;
+
+  @JsonKey(name: 'cocktails')
+  final List<Cocktail> cocktails;
+
+  factory Cocktails.fromJson(Map<String, dynamic> json) => _$CocktailsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CocktailsToJson(this);
+}
+
+@JsonSerializable()
+class Cocktail {
+
+  Cocktail(
+      {this.cocktailId,
+      this.cocktailName,
+      this.cocktailNameEnglish,
+      this.baseName,
+      this.techniqueName,
+      this.tasteName,
+      this.styleName,
+      this.alcohol,
+      this.topName,
+      this.glassName,
+      this.typeName,
+      this.cocktailDigest,
+      this.cocktailDesc,
+      this.recipeDesc,
+      this.recipes});
+
+  @JsonKey(name: 'cocktail_id')
+  final int cocktailId;
+
+  @JsonKey(name: 'cocktail_name')
+  final String cocktailName;
+
+  @JsonKey(name: 'cocktail_name_english')
+  final String cocktailNameEnglish;
+
+  @JsonKey(name: 'base_name')
+  final String baseName;
+
+  @JsonKey(name: 'technique_name')
+  final String techniqueName;
+
+  @JsonKey(name: 'taste_name')
+  final String tasteName;
+
+  @JsonKey(name: 'style_name')
+  final String styleName;
+
+  @JsonKey(name: 'alcohol')
+  final int alcohol;
+
+  @JsonKey(name: 'top_name')
+  final String topName;
+
+  @JsonKey(name: 'glass_name')
+  final String glassName;
+
+  @JsonKey(name: 'type_name')
+  final String typeName;
+
+  @JsonKey(name: 'cocktail_digest')
+  final String cocktailDigest;
+
+  @JsonKey(name: 'cocktail_desc')
+  final String cocktailDesc;
+
+  @JsonKey(name: 'recipe_desc')
+  final String recipeDesc;
+
+  @JsonKey(name: 'recipes')
+  final List<Recipe> recipes;
+
+  factory Cocktail.fromJson(Map<String, dynamic> json) => _$CocktailFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CocktailToJson(this);
+}
+
+@JsonSerializable()
+class Recipe {
+  Recipe({this.ingredientId, this.ingredientName, this.amount, this.unit});
+
+  @JsonKey(name: 'ingredient_id')
+  final int ingredientId;
+
+  @JsonKey(name: 'ingredient_name')
+  final String ingredientName;
+
+  /// 適量 or FullUP の場合は null を返却する
+  @JsonKey(name: 'amount')
+  final String amount;
+
+  /// 単位として返却しうる値: ml,tsp.,dash,枚,個,適量,FullUP,glass
+  @JsonKey(name: 'unit')
+  final String unit;
+
+  factory Recipe.fromJson(Map<String, dynamic> json) => _$RecipeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RecipeToJson(this);
+}
---
+++ lib/data/api/entity/Cocktails.g.dart
@@ -0,0 +1,84 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'Cocktails.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Cocktails _$CocktailsFromJson(Map<String, dynamic> json) {
+  return Cocktails(
+    status: json['status'] as String,
+    totalPages: json['total_pages'] as int,
+    currentPage: json['current_page'] as int,
+    cocktails: (json['cocktails'] as List)
+        ?.map((e) =>
+            e == null ? null : Cocktail.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
+}
+
+Map<String, dynamic> _$CocktailsToJson(Cocktails instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'total_pages': instance.totalPages,
+      'current_page': instance.currentPage,
+      'cocktails': instance.cocktails,
+    };
+
+Cocktail _$CocktailFromJson(Map<String, dynamic> json) {
+  return Cocktail(
+    cocktailId: json['cocktail_id'] as int,
+    cocktailName: json['cocktail_name'] as String,
+    cocktailNameEnglish: json['cocktail_name_english'] as String,
+    baseName: json['base_name'] as String,
+    techniqueName: json['technique_name'] as String,
+    tasteName: json['taste_name'] as String,
+    styleName: json['style_name'] as String,
+    alcohol: json['alcohol'] as int,
+    topName: json['top_name'] as String,
+    glassName: json['glass_name'] as String,
+    typeName: json['type_name'] as String,
+    cocktailDigest: json['cocktail_digest'] as String,
+    cocktailDesc: json['cocktail_desc'] as String,
+    recipeDesc: json['recipe_desc'] as String,
+    recipes: (json['recipes'] as List)
+        ?.map((e) =>
+            e == null ? null : Recipe.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
+}
+
+Map<String, dynamic> _$CocktailToJson(Cocktail instance) => <String, dynamic>{
+      'cocktail_id': instance.cocktailId,
+      'cocktail_name': instance.cocktailName,
+      'cocktail_name_english': instance.cocktailNameEnglish,
+      'base_name': instance.baseName,
+      'technique_name': instance.techniqueName,
+      'taste_name': instance.tasteName,
+      'style_name': instance.styleName,
+      'alcohol': instance.alcohol,
+      'top_name': instance.topName,
+      'glass_name': instance.glassName,
+      'type_name': instance.typeName,
+      'cocktail_digest': instance.cocktailDigest,
+      'cocktail_desc': instance.cocktailDesc,
+      'recipe_desc': instance.recipeDesc,
+      'recipes': instance.recipes,
+    };
+
+Recipe _$RecipeFromJson(Map<String, dynamic> json) {
+  return Recipe(
+    ingredientId: json['ingredient_id'] as int,
+    ingredientName: json['ingredient_name'] as String,
+    amount: json['amount'] as String,
+    unit: json['unit'] as String,
+  );
+}
+
+Map<String, dynamic> _$RecipeToJson(Recipe instance) => <String, dynamic>{
+      'ingredient_id': instance.ingredientId,
+      'ingredient_name': instance.ingredientName,
+      'amount': instance.amount,
+      'unit': instance.unit,
+    };
