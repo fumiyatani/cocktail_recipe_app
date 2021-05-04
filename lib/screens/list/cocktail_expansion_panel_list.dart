d13a2ea3476b6cfbad0936d40edff527c1fa9715
tanifumiya <fumiyadeveloper@gmail.com>
2021-05-04 19:26:55 +0900

    不要なファイルを削除

--- lib/screens/list/cocktail_expansion_panel_list.dart
+++
@@ -1,8 +0,0 @@
-
-import 'package:cocktail_recipe_app/screens/list/cocktail_expansion_panel_item.dart';
-
-class CocktailExpansionPanelList {
-  CocktailExpansionPanelList();
-
-  List<CocktailExpansionPanelItem> cocktailExpansionPanelList = [];
-}
