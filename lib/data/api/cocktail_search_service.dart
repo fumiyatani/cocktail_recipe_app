b364e3d54ded2e03f4de0eb55f58764f769bd185
tanifumiya <fumiyadeveloper@gmail.com>
2021-05-17 04:43:46 +0900

    静的解析の修正:CocktailSearchService.dart

--- lib/data/api/cocktail_search_service.dart
+++ lib/data/api/cocktail_search_service.dart
@@ -7,20 +7,27 @@ import 'cocktails_json_converter.dart';
 
 part 'cocktail_search_service.chopper.dart';
 
-final String _cocktailBaseUrl = "https://cocktail-f.com";
+final String _cocktailBaseUrl = 'https://cocktail-f.com';
 
 /// カクテル情報を取得するAPI定義
-@ChopperApi(baseUrl: "/api/v1")
+@ChopperApi(baseUrl: '/api/v1')
 abstract class CocktailSearchService extends ChopperService {
   static CocktailSearchService create([ChopperClient? client]) => _$CocktailSearchService(client);
 
   /// カクテル一覧を取得
-  @Get(path: "/cocktails")
-  Future<Response<Cocktails>> searchCocktails(@Query("word") String word,
-      {@Query("page") int page = 1, @Query("limit") int limit = 100});
+  @Get(path: '/cocktails')
+  Future<Response<Cocktails>> searchCocktails(
+    @Query('word') String word, {
+    @Query('page') int page = 1,
+    @Query('limit') int limit = 100,
+  });
 }
 
 /// Chopper のインスタンスを生成するためのメソッド
 ChopperClient createCocktailChopper() {
-  return ChopperClient(baseUrl: _cocktailBaseUrl, converter: CocktailsJsonConverter(), interceptors: [HttpLoggingInterceptor()]);
+  return ChopperClient(
+    baseUrl: _cocktailBaseUrl,
+    converter: CocktailsJsonConverter(),
+    interceptors: <dynamic>[HttpLoggingInterceptor()],
+  );
 }
